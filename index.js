b4660506d18b93eccfbd23e39a97cf8c30ffd514
AsaduzzamanSohun <asaduzzamansohun5@gmail.com>
2024-04-28 03:32:18 +0600

    delete method Implemented

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors());
 app.use(express.json());
@@ -28,10 +28,29 @@ async function run() {
 
         const craftCollection = client.db("craftDB").collection('craft');
 
+        app.get('/crafts', async(req, res) => {
+            const cursor = craftCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+
         app.post('/crafts', async(req, res) => {
             const item = req.body;
-            console.log(item);
+            // console.log(item);
+            const result = await craftCollection.insertOne(item);
+            res.send(result);
         })
+
+
+        app.delete('/crafts/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)};
+            const result = await craftCollection.deleteOne(query);
+            res.send(result)
+        })
+
+
 
 
 
